59fd880cff60e66f680477d5beeb7c4fb40a42bb
Bibek Timsina <timsinabibek960@gmail.com>
2022-06-16 22:06:02 +0545

    Updates

--- theme.js
+++ theme.js
@@ -1,6 +1,8 @@
 (function initTheme() {
   var theme = localStorage.getItem("theme") || "light";
   if (theme === "dark") {
+    console.log("dark theme");
     document.querySelector("html").classList.add("dark");
+    document.querySelector("html").style.setProperty("color-scheme", "dark");
   }
 })();
