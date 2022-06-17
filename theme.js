28e74b14410a95f2bddf8583a91a4ad83392a096
Bibek Timsina <timsinabibek960@gmail.com>
2022-06-17 12:20:34 +0545

    Updates

--- theme.js
+++ theme.js
@@ -1,5 +1,6 @@
 (function initTheme() {
   var theme = localStorage.getItem("theme") || "light";
+  console.log("theme", theme);
   if (theme === "dark") {
     console.log("dark theme");
     document.querySelector("html").classList.add("dark");
