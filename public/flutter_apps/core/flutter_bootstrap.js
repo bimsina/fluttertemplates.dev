c567893e978f9c31c2bd2d444b798df86969f82e
Bibek Timsina <timsinabibek960@gmail.com>
2026-07-02 21:02:37 +0545

    pnpm updates

--- public/flutter_apps/core/flutter_bootstrap.js
+++ public/flutter_apps/core/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3566416103" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4262994755" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
